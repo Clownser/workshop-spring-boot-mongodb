41c3e50cdf221dc7c8ee69d1f6a28150f1090b40
Taiwan Marinho <clownser1994@gmail.com>
2020-06-24 18:33:56 -0300

    Primeiro commit - prjeto criado

---
+++ src/test/java/com/clownser/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clownser.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
