2f0d8e85a629ec40bf424006767e487e84988550
Taiwan Marinho <clownser1994@gmail.com>
2020-06-26 19:32:25 -0300

    obtendo um post por id

---
+++ src/main/java/com/clownser/workshopmongo/resourcers/PostResource.java
@@ -0,0 +1,31 @@
+package com.clownser.workshopmongo.resourcers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.clownser.workshopmongo.domain.Post;
+import com.clownser.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> finById(@PathVariable String id) {
+		Post obj = service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+
+
+
+}
